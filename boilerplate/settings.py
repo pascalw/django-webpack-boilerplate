ed4f53c4bc62599ecc1d571a699f2dc207594f9e
Pascal Widdershoven <pascal@kabisa.nl>
2020-04-18 21:43:36 +0200

    Disable database

--- boilerplate/settings.py
+++ boilerplate/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'boilerplate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
